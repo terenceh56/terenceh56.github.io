a9dc36b5fd309e85e051652b428e6d7f42f1bbbb
terenceh56 <terencehsu56@gmail.com>
2025-11-16 19:57:42 +0800

    Still not perfect, but i'll first commit

--- src/consts.ts
+++ src/consts.ts
@@ -30,7 +30,7 @@ export const site = {
   avatar: '/avatar.png', // required
   url: 'https://github.com/terenceh56', // required
   baseUrl: '/terenceh56.github.io', // When using GitHubPages, you must enter the repository name startWith '/'. e.g. '/astro-blog'
-  motto: `"Don't think of what you want to be, think of what you want to do." -Richard P. Feynman`,
+  motto: ` 'Don't think of what you want to be, think of what you want to do.' -Richard P. Feynman`,
   recentBlogSize: 5,
   archivePageSize: 25,
   postPageSize: 10,
--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,21 +1,25 @@
 import {getCollectionByName} from "../utils/getCollectionByName.js";
 
-async function getBlogs(context) {
-  const blog = await getCollectionByName('blog')
-  return blog.map(blog => {
-    return {
-      slug: blog.slug,
-      title: blog.data.title,
-      description: blog.data.description,
-      date: blog.data.date,
-      category: blog.data.category,
-      tags: blog.data.tags,
-    }
-  })
+async function getAllPosts() {
+  const collections = ['blog', 'notes', 'thoughts', 'papers'];
+  let posts = [];
+  for (const name of collections) {
+    const items = await getCollectionByName(name);
+    posts = posts.concat(items.map(item => ({
+      slug: item.slug,
+      title: item.data.title,
+      description: item.data.description,
+      date: item.data.date,
+      category: item.data.category,
+      tags: item.data.tags,
+      collection: name
+    })));
+  }
+  return posts;
 }
 
 export async function GET({}) {
-  return new Response(JSON.stringify(await getBlogs()), {
+  return new Response(JSON.stringify(await getAllPosts()), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
